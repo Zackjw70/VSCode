5cd45c057ce4a8828ae0f6904120872d6e9c950d
Zachary Wyeth <[zjwyeth@email.neit.edu]>
2023-08-16 14:46:07 -0400

    SWAP COLORS

--- SE251/se251pong_w3_start_files/js/options.js
+++ SE251/se251pong_w3_start_files/js/options.js
@@ -31,6 +31,29 @@ var changeStraight = document.querySelectorAll(`.s`)
 var sideSelect = document.querySelector(`.sides`)
 var swapMode = document.querySelector(`.switchcolor`)
 
+function invertColor(hex){
+    if (hex.indexOf(`#`) === 0){
+        hex = hex.slice(1);
+    }
+    if (hex.length === 3){
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
+    }
+    if (hex.length !==6){
+        throw new Error('Invalid Hex Color.');
+    }
+    var r = (255 - parseInt(hex.slice(0,2), 16)).toString(16),
+        g = (255 - parseInt(hex.slice(2,4), 16)).toString(16),
+        b = (255 - parseInt(hex.slice(4,6), 16)).toString(16);
+    return '#' + padZero(r) + padZero(g) + padZero(b);
+}
+
+function padZero(str,len){
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+
+}
+
 function options(i)
 {
     player[i].fill = changeFill[i].value;
@@ -49,8 +72,26 @@ swapMode.addEventListener(`click`, function(){
     playerswapcolor = document.querySelectorAll(`.op`)
     playerswapcolor[0].classList.toggle("textdark")
     playerswapcolor[1].classList.toggle("textdark")
+    console.log(player[0].fill)
+
+    
+    var opPadColor1 = invertColor(player[0].fill)
+    var opStrokeColor1 = invertColor(pad[0].stroke)
+    var opPadColor2 = invertColor(player[1].fill)
+    var opStrokeColor2 = invertColor(pad[1].stroke)
+    
+    console.log(opPadColor1)
+
+    changeStroke[0].value = opStrokeColor1
+    changeStroke[1].value = opStrokeColor2
+
+    changeFill[0].value = opPadColor1
+    changeFill[1].value = opPadColor2
+
 
     document.querySelector(`body`).classList.toggle("backgroundlight")
+    options(1)
+    options(0)
 })
 
 
